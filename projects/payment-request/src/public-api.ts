57365b61678ef041c9c0c46d5847090829e4ed9a
MarsiBarsi <darragon-nn@yandex.ru>
2020-01-27 09:57:25 +0300

    chore(support-token): back

--- projects/payment-request/src/public-api.ts
+++ projects/payment-request/src/public-api.ts
@@ -13,5 +13,6 @@ export * from './services/payment-request.service';
 /* Tokens */
 export * from './tokens/payment-methods';
 export * from './tokens/payment-options';
+export * from './tokens/payment-request-support';
 
 export * from './module';
