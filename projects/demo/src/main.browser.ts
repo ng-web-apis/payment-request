1f74a47df54004b9c69b7496151993ac15304635
MarsiBarsi <darragon-nn@yandex.ru>
2020-01-29 12:07:05 +0300

    chore(demo): enable prod mode

--- projects/demo/src/main.browser.ts
+++ projects/demo/src/main.browser.ts
@@ -1,7 +1,10 @@
 import './polyfills';
 
+import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppBrowserModule} from './app/app.browser.module';
+
+enableProdMode();
 
 platformBrowserDynamic()
     .bootstrapModule(AppBrowserModule)
