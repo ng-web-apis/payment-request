1f4faf9e488ad4ca27ef4ddb754638f9531f003c
waterplea <alexander@inkin.ru>
2020-01-30 12:02:40 +0300

    chore(demo): update demo to look better

--- projects/demo/src/app/samples/service.sample.ts
+++ projects/demo/src/app/samples/service.sample.ts
@@ -1,6 +1,7 @@
 export const SERVICE_SAMPLE = `import {PaymentRequestService} from '@ng-web-apis/payment-request';
 
-...
+// ...
+
 constructor(private readonly paymentRequest: PaymentRequestService) {}
 
 pay(details: PaymentDetailsInit) {
