2e7f42679a60ab3b91fe23cee5842168d343f23d
MarsiBarsi <darragon-nn@yandex.ru>
2020-02-02 16:19:51 +0300

    chore(demo): apple pay and codesandbox fallback

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {PAYMENT_METHODS} from '@ng-web-apis/payment-request';
 import {DIRECTIVES_SAMPLE} from './samples/directives.sample';
 import {SERVICE_SAMPLE} from './samples/service.sample';
 
@@ -7,6 +8,29 @@ import {SERVICE_SAMPLE} from './samples/service.sample';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [
+        {
+            provide: PAYMENT_METHODS,
+            useValue: [
+                {
+                    // Apple Pay sample will start in Safari
+                    supportedMethods: 'https://apple.com/apple-pay',
+                    data: {
+                        version: 3,
+                        merchantIdentifier: 'merchant.com.example',
+                        merchantCapabilities: [
+                            'supports3DS',
+                            'supportsCredit',
+                            'supportsDebit',
+                        ],
+                        supportedNetworks: ['amex', 'discover', 'masterCard', 'visa'],
+                        countryCode: 'US',
+                    },
+                },
+                {supportedMethods: 'basic-card'},
+            ],
+        },
+    ],
 })
 export class AppComponent {
     readonly samples = {
